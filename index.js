e0eabcdfb2b7b358a30eda4fdeca24fbf6c927ae
TatsuKenshi <rockinbg@gmail.com>
2022-09-16 01:12:59 +0200

    info section overhaul, switch to Material icons

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const burger = document.querySelector(".burger");
 const narrowMenu = document.querySelector(".narrow-menu");
 const narrowMenuItems = document.querySelectorAll(".narrow-menu-item");
+const categoryButtons = document.querySelectorAll(".card-btn");
+const infoSectionItems = document.querySelectorAll(".info-section");
+
+console.log(categoryButtons);
 
 burger.addEventListener("click", function () {
   narrowMenu.classList.toggle("no-display");
@@ -9,5 +13,21 @@ burger.addEventListener("click", function () {
 narrowMenuItems.forEach((item) => {
   item.addEventListener("click", function () {
     narrowMenu.classList.toggle("no-display");
+  });
+});
+
+categoryButtons.forEach((item) => {
+  item.addEventListener("click", function (e) {
+    infoSectionItems.forEach((item) => {
+      item.classList.add("no-display");
+    });
+
+    let newInfoSectionItems = [...infoSectionItems];
+
+    const showCategory = newInfoSectionItems.find((infoItem) => {
+      return infoItem.id === e.target.innerText.toLowerCase();
+    });
+
+    showCategory.classList.remove("no-display");
   });
 });
