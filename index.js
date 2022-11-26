a9690d399018cd791e1ad09b34127cd7cc008fb7
TatsuKenshi <rockinbg@gmail.com>
2022-11-26 19:23:08 +0100

    adds scroll to top button

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const narrowMenu = document.querySelector(".narrow-menu");
 const narrowMenuItems = document.querySelectorAll(".narrow-menu-item");
 const categoryButtons = document.querySelectorAll(".card-btn");
 const infoSectionItems = document.querySelectorAll(".info-section");
-
-console.log(categoryButtons);
+const homeBtn = document.querySelector(".back-home-btn");
+const homeBtnImg = document.querySelector("#homeIcon");
 
 burger.addEventListener("click", function () {
   narrowMenu.classList.toggle("no-display");
@@ -29,5 +29,27 @@ categoryButtons.forEach((item) => {
     });
 
     showCategory.classList.remove("no-display");
+  });
+});
+
+window.onscroll = function () {
+  visibleScrollButton();
+};
+
+const visibleScrollButton = () => {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    homeBtn.classList.remove("no-display");
+  } else {
+    homeBtn.classList.add("no-display");
+  }
+};
+
+homeBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
   });
 });
